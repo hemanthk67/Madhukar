0fc6961a4f2cbe6aad4b10a51ddb8a004d0d203b
madhukar thota <sreemadhukar@gmail.com>
2020-04-21 21:46:54 +0530

    adding calander component

--- src/app/common-components/common-components.module.ts
+++ src/app/common-components/common-components.module.ts
@@ -4,10 +4,21 @@ import { PdfViewComponent } from "./internal/pdf-view/pdf-view.component";
 import { PdfPreviewComponent } from "./internal/pdf-preview/pdf-preview.component";
 import { MatIconModule } from "@angular/material/icon";
 import { LogoComponent } from "./logo/logo.component";
+import { CalenderComponent } from "./internal/calender/caleander.component";
 
 @NgModule({
-  declarations: [PdfViewComponent, PdfPreviewComponent, LogoComponent],
+  declarations: [
+    PdfViewComponent,
+    PdfPreviewComponent,
+    LogoComponent,
+    CalenderComponent
+  ],
   imports: [CommonModule, MatIconModule],
-  exports: [PdfViewComponent, PdfPreviewComponent, LogoComponent]
+  exports: [
+    PdfViewComponent,
+    PdfPreviewComponent,
+    LogoComponent,
+    CalenderComponent
+  ]
 })
 export class CommonComponentsModule {}
