016ee7e5f74cbd7bbf3c446e8600ef8965ef4c04
madhukar <sreemadhukar@gmail.com>
2021-08-20 00:42:42 +0530

    adding test-reports pages to the production

--- src/app/internal-view/production/production.module.ts
+++ src/app/internal-view/production/production.module.ts
@@ -6,9 +6,10 @@ import { ProductionComponent } from "./production.component";
 import { ProductionListComponent } from "./production-list/production-list.component";
 
 import { MatIconModule } from "@angular/material/icon";
+import { TestReportsComponent } from './test-reports/test-reports.component';
 
 @NgModule({
-  declarations: [ProductionComponent, ProductionListComponent],
+  declarations: [ProductionComponent, ProductionListComponent, TestReportsComponent],
   imports: [CommonModule, MatIconModule, ProductionRoutingModule],
 })
 export class ProductionModule {}
